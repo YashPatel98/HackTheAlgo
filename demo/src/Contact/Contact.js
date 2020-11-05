c487608e384087a59c1499f4622d1504b74069eb
payal chitroda <payalk.chitroda@gmail.com>
2020-11-05 23:48:45 +0530

    contact us

--- demo/src/Contact/Contact.js
+++ demo/src/Contact/Contact.js
@@ -9,21 +9,21 @@ class Contact extends React.Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
-    
-    onSubmit(event){
-        
-            let query = `&entry.1238625935=${event.target.name.value}&entry.941955885=${event.target.email.value}&entry.1797898034=${event.target.message.value}`;
-            let url = `https://docs.google.com/forms/d/1VuGYBKFjIxt5Nn1qsYQd_cWf5d9yc2EEwN01kkjV4AQ/formResponse?${query}`;
-            
-            fetch(url, {
-                mode: "no-cors"
-            }).then((res) => {
-              
-            });
-        alert("Successfully sent data");
-       
 
-       
+    onSubmit(event) {
+
+        let query = `&entry.1238625935=${event.target.name.value}&entry.941955885=${event.target.email.value}&entry.1797898034=${event.target.message.value}`;
+        let url = `https://docs.google.com/forms/d/1VuGYBKFjIxt5Nn1qsYQd_cWf5d9yc2EEwN01kkjV4AQ/formResponse?${query}`;
+
+        fetch(url, {
+            mode: "no-cors"
+        }).then((res) => {
+
+        });
+        alert("Successfully sent data");
+
+
+
     }
 
     render() {
@@ -32,30 +32,30 @@ class Contact extends React.Component {
             <div style={{ height: "670px" }}>
 
                 <br />  <br />  <br />
-                <h3 style={{ textAlign:"left", padding:30}}>Contact Us</h3>
+                <h3 style={{ padding: 30 }}>Contact Us</h3>
                 <br />
-                <div style={{ borderStyle: "inset", width: "80%", backgroundColor: "LightGray", marginLeft: "30px", padding: "40px" }}>
-                <form name="form" id="form" ref={form => this.form = form} enctype="text/plain" target="hidden_iframe" onSubmit={this.onSubmit}>
-                        <div class="row">
+                <div style={{ borderStyle: "inset", width: "40%", backgroundColor: "#79b6ca", marginLeft: "600px", padding: "40px" }}>
+                    <form name="form" id="form" ref={form => this.form = form} enctype="text/plain" target="hidden_iframe" onSubmit={this.onSubmit}>
+                        <div class="row" style={{ marginLeft:"60px" }} >
                             <div class="column">
-                                Name: <input type="text" id="entry.1238625935" name="name" size="25" /><br /><br />
-                        Email: <input type="text" id="entry.941955885" name="email" size="25" /><br /><br />
-                        Message:<textarea id="entry.1797898034" name="message"rows="4" cols="28" /><br /><br /><br /><br /><br /><br />
+                            <b>Name:</b> <input type="text" id="entry.1238625935" name="name" size="50" style={{marginLeft:"20px" }}/><br /><br />
+                            <b>  Email:</b> <input type="text" id="entry.941955885" name="email" size="50" style={{marginLeft:"20px" }}/><br /><br />
+                            <b>  Message:</b><textarea id="entry.1797898034" name="message" rows="4" cols="50" style={{marginLeft:"20px" }}/><br /><br /><br /><br /><br /><br />
                             </div>
-                            <br/>
-                            <br/>
-                           
+                            <br />
+                            <br />
+
                         </div>
-                        <div className="button1"><input type="submit" value="Send"/></div>
-                        
+                        <div className="button1"><input type="submit" value="Send"  /></div>
+
                     </form>
-                   
+
                 </div>
-                <iframe title="iframe" name="hidden_iframe" id="hidden_iframe" style={{display:"none"}} onload=""></iframe>
+                <iframe title="iframe" name="hidden_iframe" id="hidden_iframe" style={{ display: "none" }} onload=""></iframe>
 
 
             </div>
-           
+
         );
 
     }
